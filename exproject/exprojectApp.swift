a7e6e9672d02cbb684cd1708a01663ff8367ee38
Arinda Rahma Dini <74896245+arindarahmadini@users.noreply.github.com>
2021-11-18 14:15:01 +0700

    Add files via upload

---
+++ exproject/exprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  exprojectApp.swift
+//  exproject
+//
+//  Created by big solution on 18/11/21.
+//
+
+import SwiftUI
+
+@main
+struct exprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
